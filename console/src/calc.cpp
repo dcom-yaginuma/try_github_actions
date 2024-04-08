cbce7d5d90b344378c7ebae1a0ab09e1da124b12
YTdigital <Yaginuma.Taishiro@digital-com.com>
2024-04-08 17:09:57 +0900

    - 不要なactionsの削除 - コードにclang-format適用

--- console/src/calc.cpp
+++ console/src/calc.cpp
@@ -1,26 +1,15 @@
 #include "../include/calc.h"
 
-calc::calc() : value(0) {
-}
+calc::calc() : value(0) {}
 
-calc::calc(int value) : value(value) {
-}
+calc::calc(int value) : value(value) {}
 
-calc::~calc() {
-}
+calc::~calc() {}
 
-int calc::add(int value) {
-    return (this->value += value);
-}
+int calc::add(int value) { return (this->value += value); }
 
-int calc::minus(int value) {
-    return (this->value -= value);
-}
+int calc::minus(int value) { return (this->value -= value); }
 
-int calc::multiple(int value) {
-    return (this->value *= value);
-}
+int calc::multiple(int value) { return (this->value *= value); }
 
-int calc::division(int value) {
-    return (this->value /= value);
-}
+int calc::division(int value) { return (this->value /= value); }
